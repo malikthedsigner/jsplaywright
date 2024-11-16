3a3eaea91013d790da4cbe0de2894cc5a2de9271
Abdulmalik Muhammad <110231662+malikthedsigner@users.noreply.github.com>
2024-11-16 12:05:00 +0100

    Ttitle Checking

--- Tests/example.spec.js
+++ Tests/example.spec.js
@@ -3,6 +3,13 @@ const { test, expect } = require("@playwright/test")
 test("First Test", async({page}) =>{
 
     await page.goto("https://www.saucedemo.com/");
-    await page.pause();
+    //await page.pause();
+
+})
+
+test("Title checker", async ({page}) =>{
+    await page.goto("https://www.saucedemo.com/");
+    await expect(page).toHaveTitle("Swag Labs");
+    await page.pause;
 
 })
