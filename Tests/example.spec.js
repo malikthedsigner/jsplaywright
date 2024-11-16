67cc8f03aff0755f9579b51509f17873ceb07b17
Abdulmalik Muhammad <110231662+malikthedsigner@users.noreply.github.com>
2024-11-16 20:07:04 +0100

    Login

--- Tests/example.spec.js
+++ Tests/example.spec.js
@@ -1,15 +1,29 @@
 const { test, expect } = require("@playwright/test")
 
-test("First Test", async({page}) =>{
+test("First Test", async ({ page }) => {
 
     await page.goto("https://www.saucedemo.com/");
     //await page.pause();
 
 })
 
-test("Title checker", async ({page}) =>{
+test("Title checker", async ({ page }) => {
     await page.goto("https://www.saucedemo.com/");
     await expect(page).toHaveTitle("Swag Labs");
     await page.pause;
 
 })
+
+test("Login", async ({ page }) => {
+
+    const usernames = ['standard_user', 'locked_out_user', 'problem_user', 'performance_glitch_user', 'error_user', 'visual_user'];
+    for (const username of usernames) {
+        await page.goto("https://www.saucedemo.com/");
+        await page.locator('#user-name').fill(username);  // Using ID
+        await page.locator('#password').fill('secret_sauce');
+        await page.getByRole('button', { name: 'Login' }).click();
+        await page.pause();
+    }
+})
+
+
